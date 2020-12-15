9bafbb9a0c784e9d63e6faf165a807498a4f03c4
wanng <nnhjgg@163.com>
2020-12-15 11:59:52 +0800

    feat:添加导航&比例尺控件

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,19 @@
+<!--主地图-->
 <template>
   <div id="first-map"></div>
 </template>
 
 <script>
+import 'mapbox-gl/dist/mapbox-gl.css';
 import mapbox from 'mapbox-gl';
 
 export default {
   name: 'Map',
+  data() {
+    return {
+      mapApp: null,
+    };
+  },
   mounted() {
     mapbox.accessToken =
       'pk.eyJ1IjoibWFyc2dpcyIsImEiOiJja2Fod2xlanIwNjJzMnhvMXBkMnNqcjVpIn0.WnxikCaN2KV_zn9tLZO77A';
@@ -16,18 +23,51 @@ export default {
       center: [120.57229, 31.28505],
       zoom: 9,
     });
-    console.log(mapApp);
+
+    //实例化导航控件
+    const nav = new mapbox.NavigationControl({
+      //是否显示指南针按钮，默认为true
+      showCompass: true,
+      //是否显示缩放按钮，默认为true
+      showZoom: true,
+    });
+    //添加导航控件，控件的位置包括'top-left', 'top-right','bottom-left' ,'bottom-right'四种，默认为'top-right'
+    mapApp.addControl(nav, 'top-left');
+
+    //实例化比例尺控件
+    var scale = new mapbox.ScaleControl({
+      //比例尺控制的最大长度,默认100，以像素为单位
+      maxWidth: 100,
+      //单位，分为'imperial', 'metric','nautical'三种类型，默认为'metric'
+      unit: 'metric',
+    });
+    //添加导航控件，控件的位置包括'top-left', 'top-right','bottom-left' ,'bottom-right'四种，默认为'bottom-left'
+    mapApp.addControl(scale, 'bottom-left');
+
+    this.mapApp = mapApp;
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 #first-map {
   padding: 0%;
   margin: 0%;
   width: 100%;
   height: 100%;
   overflow: hidden;
+}
+#mouse-position {
+  top: 20px;
+  right: 20px;
+  color: blue;
+  position: absolute;
+  bottom: 5px;
+  z-index: 1;
+  font-size: 16px;
+}
+a.mapboxgl-ctrl-logo,
+.mapboxgl-ctrl-attrib {
+  display: none;
 }
 </style>
